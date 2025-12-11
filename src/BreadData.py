c7861cacbaab0a602d672a0306f34127de70e6cb
Xin Wang <Xin20Wang@outlook.com>
2025-12-11 23:54:46 +0100

    Set items ready for use

--- src/BreadData.py
+++ src/BreadData.py
@@ -14,8 +14,15 @@ class BreadData(Dataset):
     def __init__(self):
         super().__init__()
 
-    def __getitem__(self, index):
-        return super().__getitem__(index)
+    def __getitem__(self, index) -> Tuple(Tensor, int):
+        # Get first image entity.
+
+        # Map label of image to prediction_idx: Frikandelbroodje -> 1 bijvoorbeeld.
+
+        # Perform transformation if applicable
+
+        # Return tuple.
+        return (0,0)
     
     def __len__(self):
         return len(self.img)
