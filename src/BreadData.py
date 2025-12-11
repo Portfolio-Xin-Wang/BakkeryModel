35ad18f69f70d28417cb5fc01cddf864f772265f
Xin Wang <Xin20Wang@outlook.com>
2025-12-11 23:54:37 +0100

    Set methods ready

--- src/BreadData.py
+++ src/BreadData.py
@@ -8,29 +8,14 @@ from pandas import DataFrame
 from PIL import Image
 
 class BreadData(Dataset):
-    labels: DataFrame
-    original_directory: str
+    labels: list 
+    img: list 
 
-    def __init__(self, labels: DataFrame, original_directory: str, transform=None):
-        self.labels = labels
-        self.transform = transform
-        self.original_directory = original_directory
+    def __init__(self):
+        super().__init__()
 
-    def __len__(self):
-        return len(self.labels)
+    def __getitem__(self, index):
+        return super().__getitem__(index)
     
-    def __getitem__(self, idx):
-        if(torch.is_tensor(idx)):
-            idx = idx.tolist()
-        # Replace with actual image_name column.
-        img_name = os.path.join(self.original_directory, self.labels.iloc[idx, 1])
-        # Should be fixed with working image reader like PIL or OpenCV
-        image = Image.open(img_name)
-        labels = self.labels.iloc[idx, 0]
-        labels = np.array([labels], dtype=float).reshape(-1, 1)
-        sample = {'image': image, 'labels': labels}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
+    def __len__(self):
+        return len(self.img)
---
+++ src/map_bread.py
@@ -0,0 +1,17 @@
+data = {
+    0: {
+        "name": "other", # Static
+        "code": "other_code", # Static
+        "label_id": 0 # Dynamic
+    },
+    1: {
+        "name": "frikandel_broodje", # Static
+        "code": "fri_brd", # Static
+        "label_id": 1 # Dynamic
+    },
+    2: {
+        "name": "koffie_broodje",  # Static
+        "code": "koff_brd", # Static
+        "label_id": 2, # Dynamic
+    }
+}
