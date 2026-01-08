f31561fe90fd55d53606f736561c1e7e499213e6
Xin Wang <Xin20Wang@outlook.com>
2026-01-08 15:18:09 +0100

    Online sketch of the endpoint is implemented

--- src/api/predict_controller.py
+++ src/api/predict_controller.py
@@ -1,7 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, Form
+from pydantic import BaseModel
+from .predict_function import live_predict
+import io
+from PIL import Image
 
 predict_router = APIRouter()
+class PredictionInputDTO(BaseModel):
+    hero: str 
 
-@predict_router.get("/predict")
-async def predict():
-    return {"message": "This is the predict endpoint."}
+# Receive a DTO: With predicted values and a image file. It should return a online prediction and current predicted value.
+# Based on the input values, it should perhaps return secondary predictions.
+@predict_router.post("/predict")
+async def predict(file: UploadFile, help: str = Form(...)):
+    print(f"Received help text: {help}")
+    image = await file.read() # Placeholder
+    img = Image.open(io.BytesIO(image))  # Example conversion
+    print("Image loaded in PIL.")
+    # Use prediction function to get results
+    end_results = live_predict(img) # Placeholder
+
+    return end_results
--- src/api/predict_function.py
+++ src/api/predict_function.py
@@ -1,28 +1,31 @@
-from torch import nn, optim
+from torch import nn, optim, load
+import torch
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
 from src.model import BreadClassifier
 from ..services.training_pipeline import TrainingPipeline
 
+LENGHT = 200
+WIDTH = 200
 
-def _transform(length: int, width: int):
-    test_transform = transforms.Compose([
-        transforms.Resize((length, width)),
+test_transform = transforms.Compose([
+        transforms.Resize((LENGHT, WIDTH)),
         transforms.ToTensor()
     ]) 
 
-    train_transforms = transforms.Compose([
-        transforms.Resize((length, width)),
+train_transforms = transforms.Compose([
+        transforms.Resize((LENGHT, WIDTH)),
         transforms.RandomAutocontrast(),
         transforms.ToTensor()
     ])
+
+def _transform():
     return train_transforms, test_transform
 
 def train_model():
-    LENGHT = 100
-    WIDTH = 100
-    train_transforms, test_transform = _transform(LENGHT, WIDTH)
+    
+    train_transforms, test_transform = _transform()
     train_set = ImageFolder("data/training_data", transform=train_transforms)
     test_set = ImageFolder("data/test_data", transform=test_transform)
       # Placeholder for loss function
@@ -33,7 +36,7 @@ def train_model():
     pipeline.execute()
 
 # Example function
-def live_predict(input_data: dict) -> dict:
+def live_predict(input_data) -> dict:
     """
     Function to make live predictions on input data.
 
@@ -43,15 +46,27 @@ def live_predict(input_data: dict) -> dict:
     Returns:
         dict: A dictionary containing the prediction results.
     """
+    PATH_MODEL = "ready_models/bakkery_model_v1.pth"
     # Placeholder for actual prediction logic
     # In a real implementation, this would involve loading a trained model
     # and using it to make predictions on the input_data.
-    
-    # For demonstration purposes, we'll return a dummy prediction.
+    imported_model = load(PATH_MODEL, weights_only=True)
+    image = test_transform(input_data)
+    model = BreadClassifier(input_shape=3, hidden_units=15, output_shape=3)
+    model.load_state_dict(imported_model)
+    ready_input = torch.unsqueeze(image, 0)
+    model.eval()
+    with torch.inference_mode():
+        output = model(ready_input)
+        print(output)
+
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+    # Get the index of the highest probability
+    confidence, index = torch.max(probabilities, 0)
     prediction_result = {
-        "input": input_data,
-        "prediction": "dummy_bread_type",
-        "confidence": 0.95
+        "input": index.item(),
+        "prediction": "dummy_bread_type", # Some mapping of prediction to business data
+        "confidence": confidence.item()
     }
     
     return prediction_result
