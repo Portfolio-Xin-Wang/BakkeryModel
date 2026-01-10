aefbe3a7e3f9aa7bbdb2c3d310ad709b3cd42f92
Xin Wang <Xin20Wang@outlook.com>
2026-01-10 21:07:10 +0100

    Removed unnecesary comments

--- src/api/predict_controller.py
+++ src/api/predict_controller.py
@@ -1,22 +1,16 @@
-from fastapi import APIRouter, UploadFile, Form
-from pydantic import BaseModel
-from .predict_function import live_predict
 import io
+
+from fastapi import APIRouter, Form, UploadFile
 from PIL import Image
 
-predict_router = APIRouter()
-class PredictionInputDTO(BaseModel):
-    hero: str 
+from .predict_function import live_predict
 
-# Receive a DTO: With predicted values and a image file. It should return a online prediction and current predicted value.
-# Based on the input values, it should perhaps return secondary predictions.
+predict_router = APIRouter()
+# This is the prdict endpoint.
 @predict_router.post("/predict")
 async def predict(file: UploadFile, help: str = Form(...)):
-    print(f"Received help text: {help}")
     image = await file.read() # Placeholder
     img = Image.open(io.BytesIO(image))  # Example conversion
-    print("Image loaded in PIL.")
-    # Use prediction function to get results
     end_results = live_predict(img) # Placeholder
 
     return end_results
