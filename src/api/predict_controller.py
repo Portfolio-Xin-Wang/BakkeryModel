6b9360025853b6645fde42d075cf9f33d740df66
XinOfWang2001 <Xin20Wang@outlook>
2026-01-13 15:19:49 +0100

    Removed unnused variable

--- src/api/predict_controller.py
+++ src/api/predict_controller.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter, Form, UploadFile
+from fastapi import APIRouter, UploadFile
 
 from .predict_function import live_predict
 
 predict_router = APIRouter()
 
 @predict_router.post("/predict")
-async def predict(file: UploadFile, help: str = Form(...)):
+async def predict(file: UploadFile):
     image: bytes = await file.read()
     end_results = live_predict(image) # Placeholder
     return end_results
